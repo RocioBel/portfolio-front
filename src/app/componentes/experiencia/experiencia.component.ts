20a96a0cf0cfa3aa495894c5076a7030753c7744
Rocio Vargas <vargas.rocio@hotmail.com.ar>
2023-05-11 23:20:35 -0300

    add funcionality in education component

--- src/app/componentes/experiencia/experiencia.component.ts
+++ src/app/componentes/experiencia/experiencia.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { PortfolioService } from 'src/app/servicios/portfolio.service';
 
 
 @Component({
@@ -8,9 +9,58 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ExperienciasComponent implements OnInit {
   @Input() experienciaList:any;
-  constructor(){
+  @Input() nuevaExperiencia:any;
+  experienciaSeleccionadaIndex: number = -1
+  modoAdicion = false;
+  tiposTrabajo = [
+    { label: 'Jornada completa', value: '1' },
+    { label: 'Jornada reducida', value: '2' },
+    { label: 'Autónomo', value: '3' }
+  ]
+  constructor(private portfolioServicio:PortfolioService){
     this.experienciaList = [];
   }
 
   ngOnInit(): void {  }
+
+  activarModoEdicion(index:any) {
+    this.experienciaSeleccionadaIndex = index;
+  }
+
+  activarModoAdicion() {
+    this.modoAdicion = true;
+    this.nuevaExperiencia = {}; 
+  }
+
+  guardarCambios(cambios:any) {
+    console.log("cambios: "+JSON.stringify(cambios));
+    this.portfolioServicio.actualizarExperiencia(cambios.experienceId, cambios).subscribe(
+      response => {
+        console.log('Datos actualizados:', response);
+      },
+      error => {
+        console.log("Error al actualizar datos:", error);
+      }
+    );
+    this.experienciaSeleccionadaIndex = -1;
+  }
+
+  agregarExperiencia(experiencia:any) {
+    console.log("cambios: "+JSON.stringify(experiencia));
+    this.portfolioServicio.agregarExperiencia(experiencia).subscribe(
+      response => {
+        console.log('Datos agregador:', response);
+        this.experienciaList.push(experiencia);
+      },
+      error => {
+        console.log("Error al agregar datos:", error);
+      }
+    );
+    
+  }
+
+  obtenerLabelTipoTrabajo(id: string): string {
+    const tipoTrabajo = this.tiposTrabajo.find(tipo => tipo.value == id);
+    return tipoTrabajo ? tipoTrabajo.label : '';
+  }
 }
